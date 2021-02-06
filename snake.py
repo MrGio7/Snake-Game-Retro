265d0fd94f35b5c0d4f65ee0d9d0c731241beeb8
MrGio7 <lashagiorgi1233@gmail.com>
2021-02-06 16:06:25 +0400

    added snake bite

--- snake.py
+++ snake.py
@@ -12,14 +12,16 @@ class Snake(Cube):
         self.update = 0
         self.posx = random.randrange(0, self.screen_width, self.screen_width / self.rows)
         self.posy = random.randrange(0, self.screen_width, self.screen_width / self.rows)
+        self.bite = False
         
     def draw_snake(self, surface, color):
         self.update += 1
         for square in self.body:
             pygame.draw.rect(surface, color, (square[0] + 1, square[1] + 1, self.size, self.size))
-
+            if self.body.count(square) > 1:
+                self.bite = True
         
-        if self.update >= 40:
+        if self.update >= 10:
             if self.direction == "Right":
                 self.pos[0] += 25
             elif self.direction == "Left":
@@ -30,6 +32,7 @@ class Snake(Cube):
                 self.pos[1] += 25
 
             self.body.append(list(self.pos))
+
             if self.pos != [self.posx, self.posy]:
                 self.body.pop(0)
             else:
@@ -37,25 +40,27 @@ class Snake(Cube):
             self.update = 0
 
     def move(self):
-        key = pygame.key.get_pressed()
+        if self.bite == False:
+            key = pygame.key.get_pressed()
 
-        if key[pygame.K_DOWN]:
-            self.direction = "Down"
-            
-        if key[pygame.K_UP]:
-            self.direction = "UP"
-            
-        if key[pygame.K_LEFT]:
-            self.direction = "Left"
-            
-        if key[pygame.K_RIGHT]:
-            self.direction = "Right"
+            if key[pygame.K_DOWN]:
+                self.direction = "Down"
+                
+            if key[pygame.K_UP]:
+                self.direction = "UP"
+                
+            if key[pygame.K_LEFT]:
+                self.direction = "Left"
+                
+            if key[pygame.K_RIGHT]:
+                self.direction = "Right"
+        elif self.bite == True:
+            pygame.quit()
 
     def random_cube(self, surface, color):
         for square in self.body:
-            if [self.posx, self.posy] != [square[0], square[1]]:
+            if [self.posx, self.posy] != [square[0], square[1]] and square != [self.posx, self.posy]:
                 self.draw_cube(surface, color)
             elif [self.posx, self.posy] == [square[0], square[1]]:
                 self.posx = random.randrange(0, self.screen_width, self.screen_width / self.rows)
                 self.posy = random.randrange(0, self.screen_width, self.screen_width / self.rows)
-                print(self.body)
